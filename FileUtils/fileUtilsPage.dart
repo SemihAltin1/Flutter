53595737985d57b610a0e82e0c71018674f04a08
SemihAltin1 <80004993+SemihAltin1@users.noreply.github.com>
2021-12-05 23:02:53 +0300

    FileUtils uploaded

---
+++ FileUtils/fileUtilsPage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+//import file_utils class
+
+class FileUtilsPage extends StatefulWidget {
+
+  @override
+  _FileUtilsPageState createState() => _FileUtilsPageState();
+}
+
+class _FileUtilsPageState extends State<FileUtilsPage> {
+  var textController = TextEditingController();
+  String text="Text";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("FileUtils")),
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: [
+          TextField(
+            controller: textController,
+            decoration: InputDecoration(labelText: "Enter Text"),
+          ),
+          ElevatedButton(
+            onPressed: (){
+                FileUtils.saveToFile(textController.text);
+            }, 
+            child: Text("Save to tile")
+            ),
+          SizedBox(height: 30),
+
+          Text(text),
+          ElevatedButton(
+            onPressed: ()async{
+                setState(() {
+                  FileUtils.readFromFile().then((value) {
+                    setState(() {
+                      text=value;
+                    });
+                  });
+                });
+            }, 
+            child: Text("Save to tile")
+            ),
+        ],
+      ),     
+    );
+  }
+}
